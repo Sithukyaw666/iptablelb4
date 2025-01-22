3dff52a5bfebe223f838461a63cdfe9b5b29fc88
zawlinhein <zawlin.hein@frontiir.net>
2025-01-22 16:56:17 +0630

    add CORS

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"github.com/sithukyaw666/iptablelb4/handler"
@@ -8,6 +9,10 @@ import (
 
 func main() {
 	router := gin.Default()
+
+	// Add CORS middleware
+	router.Use(cors.Default())
+
 	handler, err := handler.NewiptableHandler()
 	if err != nil {
 		logrus.Error("Can't initialize iptables instance", err)
